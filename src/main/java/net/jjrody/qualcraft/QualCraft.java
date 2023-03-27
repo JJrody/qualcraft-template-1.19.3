b2ec02bfa5741e9a808f74b66b16738a25115ac2
Julian Rodriguez <jjrod928@gmail.com>
2023-03-27 14:58:56 -0500

    Initial commit

---
+++ src/main/java/net/jjrody/qualcraft/QualCraft.java
@@ -0,0 +1,17 @@
+package net.jjrody.qualcraft;
+
+import net.fabricmc.api.ModInitializer;
+
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
+
+public class QualCraft implements ModInitializer {
+	public static final String MOD_ID = "qualcraft";
+	public static final Logger LOGGER = LoggerFactory.getLogger(MOD_ID);
+
+	@Override
+	public void onInitialize() {
+
+
+	}
+}
